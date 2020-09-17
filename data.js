033c5c178eca754a25fd16197cf9bffd8ff70d5e
susird <susir95@gmail.com>
2020-09-16 21:52:39 -0500

    initial commit

---
+++ data.js
@@ -0,0 +1,78 @@
+export const data = {
+  "header": {
+    "logo": {
+      "title": "Crasus",
+      "img": "https://img.freepik.com/foto-gratis/disposicion-rectangular-suculentas-cactus-suculentas-plantador_1127-3108.jpg?size=626&ext=jpg" 
+    },
+    "links": [
+      {
+        "label": "Inicio",
+        "href": "https://api.adorable.io/avatars/1000/100.png"
+      },
+      {
+        "label": "Publicaciones",
+        "href": "https://api.adorable.io/avatars/1000/05.png"
+      },
+      {
+        "label": "Fotos",
+        "href": "https://api.adorable.io/avatars/1000/200.png"
+      },
+      {
+        "label": "Plantas",
+        "href": "https://api.adorable.io/avatars/1000/300.png"
+      }
+    ]
+  },
+  "main": {
+    "aside": [
+      {
+        "title": "Lorem Ipsum?",
+        "content": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+      },
+
+      {
+        "title": "Where can I get some?",
+        "content": "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."
+      }
+    ],
+    
+    "article": [
+      {
+       "image": "https://api.adorable.io/avatars/1000/65.png",
+       "description": "Lorem Ipsum1"
+      },
+      {
+       "image": "https://api.adorable.io/avatars/1000/85.png",
+        "description": "Lorem Ipsum2"
+      },
+      {
+        "image": "https://api.adorable.io/avatars/1000/65.png",
+        "description": "Lorem Ipsum3"
+      }
+    ]
+    },
+  
+  "footer": {
+    "logo": {
+      "title": "Crassus"
+    },
+    "description": "Especialistas y enamorados de las Crasas.",
+    "social-media": [
+      {
+        "name": "Instagram",
+        "href": "https://instagram.com/ve5pula",
+        "image": "https://api.adorable.io/avatars/1000/35.png"
+      },
+      {
+        "name": "Twitter",
+        "href": "https://twitter.com/ve5pula",
+        "image": "https://api.adorable.io/avatars/1000/15.png"
+      },
+      {
+        "name": "Facebook",
+        "href": "https://facebook.com",
+        "image": "https://api.adorable.io/avatars/1000/25.png"
+      }
+    ]
+  }
+}
