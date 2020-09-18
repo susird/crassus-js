1a0e6697ade4cf5d2ea608ac370c87a9d5989dda
susird <susir95@gmail.com>
2020-09-18 15:33:57 -0500

    Add header component

---
+++ Components/Header.js
@@ -0,0 +1,26 @@
+import { data } from '../data.js'
+
+export function createHeader() {
+  const { header: {links} } = data
+  const headerElement = document.createElement('header');
+  headerElement.className = 'header';
+
+  const linkElement = document.createElement('ul');
+  linkElement.className = 'links';
+  
+  links.forEach( ({href, label})  => {
+    const element = document.createElement('li');
+    element.className = 'nav-links';
+    element.innerHTML = `
+    <a href= ${href}>
+    ${label}
+    </a>
+    `;
+
+    linkElement.appendChild(element);
+  })
+
+  headerElement.appendChild(linkElement);
+
+  return headerElement
+}
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import { createLogo } from './Components/Logo.js'
+import { createHeader } from './Components/Header.js'
 
 const rootElement = document.getElementById('root');
+const rootElement = document.getElementById('root');
+
+const headerElement = createHeader();
 const logoElement = createLogo();
 
+rootElement.appendChild(headerElement);
 rootElement.appendChild(logoElement);
