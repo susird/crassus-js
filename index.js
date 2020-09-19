2edf373b553f95e5ccf6086a4f12d62497096b84
susird <susir95@gmail.com>
2020-09-18 22:01:02 -0500

    Add logo component and style

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 console.log('Hello World')
+
+import { createLogo } from './Components/Logo.js'
+
+const rootElement = document.getElementById('root');
+const logoElement = createLogo();
+
+rootElement.appendChild(logoElement);
