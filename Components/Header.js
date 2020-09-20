b4fb403b5a2cec1e7956f6e8639c221f4e81a7a0
susird <susir95@gmail.com>
2020-09-20 17:08:52 -0500

    fix links styles

--- Components/Header.js
+++ Components/Header.js
@@ -1,4 +1,5 @@
 import { data } from '../data.js';
+import { createLogo } from './Logo.js';
 
 export function createHeader() {
   const { header: {links} } = data;
@@ -7,19 +8,17 @@ export function createHeader() {
 
   const linkElement = document.createElement('ul');
   linkElement.className = 'links';
-  
-  links.forEach( ({href, label})  => {
+
+  links.forEach(({href, label})  => {
     const element = document.createElement('li');
     element.className = 'nav-links';
-    element.innerHTML = `
-    <a href= ${href}>
-    ${label}
-    </a>
-    `;
+    element.innerHTML = `<a href=${href}>${label}</a>`;
 
     linkElement.appendChild(element);
+    
   });
-
+  const logoElement = createLogo();
+  headerElement.appendChild(logoElement);
   headerElement.appendChild(linkElement);
 
   return headerElement;
--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-import { createLogo } from './Components/Logo.js'
-import { createHeader } from './Components/Header.js'
+import { createHeader } from './Components/Header.js';
 
 const rootElement = document.getElementById('root');
 const headerElement = createHeader();
-const logoElement = createLogo();
+
 
 rootElement.appendChild(headerElement);
-rootElement.appendChild(logoElement);
+
