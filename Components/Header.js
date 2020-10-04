4134b5a1a61447c2cc741aaf17feaad7fe93c432
susird <susir95@gmail.com>
2020-10-03 23:39:34 -0500

    Delete repeated code

--- Components/Header.js
+++ Components/Header.js
@@ -2,13 +2,13 @@ import { data } from '../data.js';
 import { createLogo } from './Logo.js';
 
 export function createHeader() {
-  const { header: {links} } = data;
+  const { header: { links } } = data;
   const headerElement = document.createElement('header');
   headerElement.className = 'header';
-  
+
   const hamElement = document.createElement('div');
   hamElement.className = 'ham-box';
-  
+
   const linkElement = document.createElement('ul');
   linkElement.className = 'links';
 
@@ -19,7 +19,7 @@ export function createHeader() {
       <div class="item-button item-button-2"></div>
       <div class="item-button item-button-3"></div>`
 
-  links.forEach(({href, label})  => {
+  links.forEach(({ href, label }) => {
     const element = document.createElement('li');
     element.className = 'nav-links';
     element.innerHTML = `<a href=${href}>${label}</a>`;
@@ -27,10 +27,10 @@ export function createHeader() {
   });
 
   buttonElement.addEventListener('click', toggleHamburger)
-   function toggleHamburger() {
-     headerElement.classList.toggle("showHam");
-   };
-   
+  function toggleHamburger() {
+    headerElement.classList.toggle("showHam");
+  };
+
   const logoElement = createLogo();
   headerElement.appendChild(logoElement);
   headerElement.appendChild(hamElement);
