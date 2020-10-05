e4a7264d9ef34533ed54d7c0ee398c245555d6a9
susird <susir95@gmail.com>
2020-10-04 23:14:19 -0500

    responsive main

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ import { createMain } from './Components/Main.js'
 
 const rootElement = document.getElementById('root');
 const headerElement = createHeader();
-const logoElement = createLogo();
 const mainElement = createMain();
 
-rootElement.appendChild(logoElement);
-rootElement.appendChild(mainElement);
 rootElement.appendChild(headerElement);
+rootElement.appendChild(mainElement);
